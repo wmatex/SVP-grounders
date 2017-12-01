109b850288ae8849e5c9c8bb0d5e35e42c3356cb
Matej Vavrinec <wmatex@gmail.com>
2017-12-01 17:37:59 +0100

    Split generation of data and rules

--- Datasets/Syntetic/create_dataset.py
+++ Datasets/Syntetic/create_dataset.py
@@ -4,6 +4,7 @@ import argparse
 import random
 import math
 import itertools
+from utils import generate_identifier
 
 random.seed(0)
 
@@ -11,40 +12,27 @@ random.seed(0)
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate syntetic dataset for grounder')
 
-    parser.add_argument('-f', '--facts', type=int, default=10,
-                        help='Number of facts')
+    parser.add_argument(
+        '-f', '--facts', type=int, default=10,
+        help='Number of facts'
+    )
 
-    parser.add_argument('-t', '--tables', type=int, default=3,
-                        help='Number of tables')
+    parser.add_argument(
+        '-t', '--tables', type=int, default=3,
+        help='Number of tables'
+    )
 
-    parser.add_argument('-r', '--relations', type=int, default=2,
-                        help='Number of connections')
+    parser.add_argument(
+        '-r', '--relations', type=int, default=2,
+        help='Number of connections'
+    )
 
-    parser.add_argument('-c', '--columns', type=int, default=3,
-                        help='Average number of table columns')
-
-    parser.add_argument('-u', '--rules', type=int, default=5,
-                        help='Number of table rules')
+    parser.add_argument(
+        '-c', '--columns', type=int, default=3,
+        help='Average number of table columns'
+    )
 
     return parser.parse_args()
-
-def generate_identifier(x, y = None):
-    a = 97
-    z = 123
-    chars = [str(chr(i)) for i in range(a, z)]
-
-    xx = 1
-    idt = ""
-    while xx > 0:
-        idt = chars[x % (z - a)] + idt
-        xx = int(x / (z - a))
-        x = xx
-
-    if y is not None:
-        return idt + str(y)
-    else:
-        return idt
-
 
 class Table:
     def __init__(self, id, columns, rows):
@@ -62,62 +50,22 @@ class Table:
         rows = len(self._data)
         return self._data[random.randint(0, rows-1)][0]
 
-class Rule:
-    def __init__(self, id, tables, rules, num_of_tables, num_of_rules):
-        self._id = generate_identifier(id)
-        self._tables = tables
-        self._rules = rules
-
-        self._generate(num_of_tables, num_of_rules)
-
-
-    def _generate(self, num_of_tables, num_of_rules):
-        num_of_tables = min(num_of_tables, len(self._tables))
-        num_of_rules = min(num_of_rules, len(self._rules))
-
-        tables = random.sample(self._tables, num_of_tables)
-        rules = random.sample(self._rules, num_of_rules)
-
-        self._rule = tables
-
-
 class Convertor:
     def convertTo(self, tables):
         pass
 
-    def convertRulesTo(self, rules):
-        pass
-
-
 class DatalogConvertor(Convertor):
     def convertTo(self, tables):
         for t in tables:
+            print("% " + t._id + ":" + str(len(t._data[0])) + ":[", end="")
+            rels = []
+            for r in t._relations:
+                rels.append(r._id)
+            print(",".join(rels) + "]")
+
+        for t in tables:
             for row in t._data:
                 print(t._id + "(" + ", ".join(row) + ").")
-
-    def convertRulesTo(self, rules):
-        for r in rules:
-            head_symbols = set()
-            for t in r._rule:
-                prefix = t._id.upper()
-                for index in range(t._columns):
-                    head_symbols.add(prefix + str(index))
-
-            print("rule_" + r._id + "(" + ", ".join(head_symbols) + ") :- ", end="")
-            table_rules = []
-            for t in r._rule:
-                prefix = t._id.upper()
-                variables = []
-                for index in range(t._columns):
-                    variables.append(prefix + str(index))
-
-                for relation in t._relations:
-                    variables.append(relation._id.upper() + "0")
-
-                table_rules.append(t._id + "(" + ", ".join(variables) + ")")
-
-            print(", ".join(table_rules) + ".")
-
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -130,6 +78,7 @@ if __name__ == "__main__":
         table = Table(t, columns, args.facts)
         tables.append(table)
 
+    random.seed(0)
     num_relations = min(args.relations, args.tables**2)
     pairs = itertools.product(tables, tables)
     pairs = random.sample(list(pairs), num_relations)
@@ -137,9 +86,5 @@ if __name__ == "__main__":
     for t1, t2 in pairs:
         t1.addRelation(t2)
 
-    for u in range(args.rules):
-        rules.append(Rule(u, tables, [], random.randint(1, args.tables), 0))
-
     convertor = DatalogConvertor()
     convertor.convertTo(tables)
-    convertor.convertRulesTo(rules)
