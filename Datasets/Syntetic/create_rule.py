e977f2bef445d48e7f0bc90d41696e265c1a49a0
Matej Vavrinec <wmatex@gmail.com>
2017-12-04 16:19:30 +0100

    Add unique, duplicity options

--- Datasets/Syntetic/create_rule.py
+++ Datasets/Syntetic/create_rule.py
@@ -28,81 +28,121 @@ def parse_arguments():
     )
 
     parser.add_argument(
-        '-d', '--duplicity', type=float, default=0.0,
-        help='Probability of having duplicate predicates in the body'
+        '-d', '--duplicity', type=int, default=0,
+        help='Number of duplicate predicates in body'
+    )
+
+    parser.add_argument(
+        '-u', '--unique', action='store_true',
+        help='Use unique variable names in duplicit tables'
+    )
+
+    parser.add_argument(
+        '-p', '--print', action='store_true',
+        help='Print the data source file to stdout'
     )
 
     return parser.parse_args()
 
 class Parser:
-    def __init__(self, data_file):
-        self._predicates = {}
-        self._parse(data_file)
+    def __init__(self, data_file, print_to_stdout):
+        self.predicates = {}
+        self._parse(data_file, print_to_stdout)
 
     def _parse_atoms(self, line):
-        
-        pass
+        atom_info_reg = re.compile('(?P<name>[\w]+):\s*(?P<arity>[0-9]+):\s*\[(?P<relations>[\w,]*)\]')
+        match = atom_info_reg.search(line)
 
-    def _parse(self, data_file):
-        last_identifier = None
-        id_reg = re.compile('^([a-z]+)\(')
-        arity_reg = re.compile(',')
+        if match:
+            predicate = {
+                'arity': int(match.group('arity')),
+                'name': match.group('name'),
+                'relations': []
+            }
 
+            for atom in match.group('relations').split(','):
+                if atom:
+                    predicate['relations'].append(atom)
+
+            self.predicates[match.group('name')] = predicate
+
+    def _parse(self, data_file, print_to_stdout):
         for line in data_file:
+            if print_to_stdout:
+                print(line, end="")
             # Comment with relations
             if line[0] == '%':
                 self._parse_atoms(line)
-            else:
-                match = id_reg.match(line)
-                if match:
-                    if last_identifier is not match.group(1):
-                        last_identifier = match.group(1)
-                        m = arity_reg.findall(line)
-                        self._predicates[last_identifier]['arity'] = len(m) + 1
 
-        print(self._predicates)
 
 
 class Rule:
-    def __init__(self, id, tables, num_of_tables):
+    def __init__(self, id, parser, num_of_tables):
         self._id = generate_identifier(id)
-        self._tables = tables
 
-        self._create(num_of_tables)
+        self._create(parser, num_of_tables)
 
-    def _create(self, num_of_tables):
-        num_of_tables = min(num_of_tables, len(self._tables))
+    def _create(self, parser, num_of_tables):
+        num_of_tables = min(num_of_tables, len(parser.predicates))
 
-        tables = random.sample(self._tables, num_of_tables)
+        tables = random.sample(list(parser.predicates), num_of_tables)
+        self._tables = [parser.predicates[t] for t in tables]
 
-        self._rule = tables
+    def _create_head(self, tables, duplicity, unique_names=False):
+        head_symbols = {}
 
-    def generate(self, duplicity):
-        head_symbols = set()
-        for t in self._rule:
-            prefix = t._id.upper()
-            for index in range(t._columns):
-                head_symbols.add(prefix + str(index))
+        for t in tables:
+            head_symbols[t['name']] = head_symbols.get(t['name'], 0) + 1
 
-        print("rule_" + self._rule._id + "(" + ", ".join(head_symbols) + ") :- ", end="")
+        heads = []
+        for name, count in head_symbols.items():
+            if unique_names:
+                for i in range(duplicity):
+                    if i > 0:
+                        heads.extend([name.upper() + str(index) + "_" + str(i) for index in range(count)])
+                    else:
+                        heads.extend([name.upper() + str(index) for index in range(count)])
+
+            else:
+                heads.append(name.upper() + "0")
+
+        return heads
+
+    def _create_body(self, prefix, arity, relations, dup, unique_names):
+        variables = []
+        suffix = ''
+        if dup > 0 and unique_names:
+            suffix = "_" + str(dup)
+
+        for index in range(arity - len(relations)):
+            variables.append(prefix + str(index) + suffix)
+
+        for r in relations:
+            variables.append(r.upper() + "0" + suffix)
+
+        return variables
+
+
+    def generate(self, width, duplicity, unique_names):
+        head_symbols = self._create_head(self._tables, duplicity, unique_names)
+        print("rule_" + self._id + "(" + ", ".join(head_symbols) + ") :- ", end="")
+
         table_rules = []
-        for t in self._rule:
-            prefix = t._id.upper()
-            variables = []
-            for index in range(t._columns):
-                variables.append(prefix + str(index))
+        for t in self._tables:
+            prefix = t['name'].upper()
 
-            for relation in t._relations:
-                variables.append(relation._id.upper() + "0")
+            for dup in range(duplicity):
+                variables = self._create_body(prefix, t['arity'], t['relations'], dup, unique_names)
 
-            table_rules.append(t._id + "(" + ", ".join(variables) + ")")
+                table_rules.append(t['name'] + "(" + ", ".join(variables) + ")")
 
         print(", ".join(table_rules) + ".")
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    parser = Parser(args.data)
+    parser = Parser(args.data, args.print)
 
-    # for i in range(args.count):
-    #     r = Rule(i, )
+    for i in range(args.count):
+        r = Rule(i, parser, args.width)
+        r.generate(args.width, args.duplicity, args.unique)
