eea27e4c138ba53198d387835fdff36c54591179
Matej Vavrinec <wmatex@gmail.com>
2018-01-22 15:48:39 +0100

    Add meta runner script

---
+++ Datasets/meta_runner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -N DP-Grounders
+#PBS -l select=1:ncpus=4:mem=16gb:scratch_local=20gb
+#PBS -l walltime=4:00:00
+#PBS -j oe
+#PBS -m e
+
+function sync_result() {
+    rsync -az experiments/grid-search/ $DATADIR/$GRID_DATA
+}
+
+trap "sync_result && clean_scratch" TERM EXIT
+
+DATADIR="/storage/praha1/home/wmatex/SVP-grounders"
+GRID_DATA="Datasets/experiments/grid-search"
+
+# Prepare data
+rsync -az $DATADIR/SVP-grounders/ $SCRATCHDIR/
+
+# Switch to the scratch dir
+cd $SCRATCHDIR/SVP-grounders/Datasets
+
+# Compute
+./grid-search -t 4 >> experiments/grid-search/result.out
+
+# Sync computed results
+sync_result || export CLEAN_SCRATCH=false # copies result to your input data directory. The result will not be removed if the copying fails.
