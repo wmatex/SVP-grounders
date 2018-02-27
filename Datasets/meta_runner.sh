88fdb1e63c028ab0e84106ed15fdfc5f2194529b
Matej Vavrinec <wmatex@gmail.com>
2018-02-27 17:02:50 +0100

    Update task parameters

--- Datasets/meta_runner.sh
+++ Datasets/meta_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -N DP-Grounders
-#PBS -l select=1:ncpus=4:mem=128gb:scratch_local=5gb
+#PBS -l select=1:ncpus=5:mem=256gb:scratch_local=20gb
 #PBS -l walltime=10:00:00
 #PBS -j oe
 #PBS -m e
